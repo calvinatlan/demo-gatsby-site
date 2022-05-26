5819220166f8fb888809367fd3a00432af6b75ec
Calvin Atlan <calvinatlan@gmail.com>
2022-05-26 15:28:24 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `Demo Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-styled-components"]
+};
